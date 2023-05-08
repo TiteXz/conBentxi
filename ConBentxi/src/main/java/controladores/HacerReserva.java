a121b39eba96e71bff396e1af870b7357e1d8288
admin <admin@DESKTOP-UJGJBG5>
2023-05-08 14:19:05 +0200

    Hacerreserva y modeloCliente

--- ConBentxi/src/main/java/controladores/HacerReserva.java
+++ ConBentxi/src/main/java/controladores/HacerReserva.java
@@ -7,6 +7,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import clases.Cliente;
+import modelo.modeloCliente;
+
 /**
  * Servlet implementation class HacerReserva
  */
@@ -28,7 +31,27 @@ public class HacerReserva extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
+		//datuak jaso
+		String dni = request.getParameter("dni");
+		String nombre = request.getParameter("nombre");
+		String apellido = request.getParameter("apellido");
+		Boolean mayor_edad = request.getParameter("mayor_edad") == null? false :true;
+
+		int num_tarjeta = Integer.parseInt(request.getParameter("num_tarjeta"));
 		
+		//insertatu
+		modeloCliente mC = new modeloCliente();
+		
+		Cliente cliente = new Cliente();
+		cliente.setDni(dni);
+		cliente.setNombre(nombre);
+		cliente.setApellido(apellido);
+		cliente.setMayor_edad(mayor_edad);
+		cliente.setNum_tarjeta(num_tarjeta);
+		
+		mC.Conectar();
+		mC.ainadirUsuario(cliente);
+		mC.cerrar();
 		
 		request.getRequestDispatcher("ReservarRuta.jsp").forward(request, response);
 	}
--- ConBentxi/src/main/java/modelo/modeloCliente.java
+++ ConBentxi/src/main/java/modelo/modeloCliente.java
@@ -1,7 +1,28 @@
 package modelo;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import clases.Cliente;
+
 public class modeloCliente extends Conexion{
 	
-	
+	public void ainadirUsuario(Cliente cliente) {
+		try {
+			
+			PreparedStatement pst = conexion.prepareStatement("INSERT INTO clientes (dni, nombre, apellido, mayor_edad, num_tarjeta) VALUES(?,?,?,?,?)");
+			pst.setString(1, cliente.getDni());
+			pst.setString(2, cliente.getNombre());
+			pst.setString(3, cliente.getApellido());
+			pst.setBoolean(4, cliente.isMayor_edad());
+			pst.setInt(5, cliente.getNum_tarjeta());
+
+			pst.execute();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
 	
 }
